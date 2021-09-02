cf8110540a0ece88fc46ebcd7a36056354f3b904
Feiyang(Vance) Chen <feiyangchen98@gmail.com>
2021-09-01 23:28:34 -0400

    update

--- os.sh
+++ os.sh
@@ -1,7 +1,11 @@
 # generate embedding.npy
 python Tumor2Graph.py
 
-# 
+# generate dna_rna_methy_cluster_result.csv
 python embedding.py
+
+# generate cluster_result.pdf
 python life_cancer.py
+
+# generate OS_complete.csv
 python generate_OS.py

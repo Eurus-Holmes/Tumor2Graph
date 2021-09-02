a0527a09b1c26823f221f4599bb1e87da9dd07b2
Feiyang(Vance) Chen <feiyangchen98@gmail.com>
2021-09-01 21:43:00 -0400

    update

--- models/gcn_conv.py
+++ models/gcn_conv.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env
-# coding:utf-8
-"""
-Created on 2020/8/25 10:55
-
-base Info
-"""
-__author__ = 'xx'
-__version__ = '1.0'
-
-
 import torch
 import torch.nn as nn
 from torch.nn import Parameter

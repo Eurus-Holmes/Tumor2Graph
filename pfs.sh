c7c9e990dc39bb69a4c70eaed5ca36ef4d10388d
Feiyang(Vance) Chen <feiyangchen98@gmail.com>
2021-09-01 23:30:15 -0400

    Create pfs.sh

---
+++ pfs.sh
@@ -0,0 +1,11 @@
+# generate embedding.npy
+python Tumor2Graph.py
+
+# generate dna_rna_methy_cluster_result.csv
+python embedding.py
+
+# generate cluster_result.pdf
+python life_cancer_pfs.py
+
+# generate PFS_complete.csv
+python generate_PFS.py

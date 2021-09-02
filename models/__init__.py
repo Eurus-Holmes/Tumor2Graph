ffefed7515f75a36d811b90fd50c8d1069c14369
Feiyang(Vance) Chen <feiyangchen98@gmail.com>
2021-09-01 21:41:27 -0400

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,1 @@
-#!/usr/bin/env
-# coding:utf-8
-"""
-Created on 2020/8/25 15:43
 
-base Info
-"""
-__author__ = 'xx'
-__version__ = '1.0'
-
-# import torch
-#
-# mat = torch.zeros([10])
-# print(torch.norm(mat))
